bc5b77ddfcd733fe67faa6cb7904f9ed2d1acd73
LoveTianyi <vocaloidluotianyi@qq.com>
2024-06-12 21:49:04 +0800

    second update

--- src/main/java/com/jxglyf/mpdemo/Controller/UserController.java
+++ src/main/java/com/jxglyf/mpdemo/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.jxglyf.mpdemo.entity.User;
 import com.jxglyf.mpdemo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,16 @@ public class UserController {
         System.out.println(list);
         return list;
 
+    }
+
+    @PostMapping("/user")
+    public String save(User user){
+
+        int i = userMapper.insert(user);
+        if(i > 0){
+            return "success";
+        }else{
+            return "fail";
+        }
     }
 }
--- src/main/java/com/jxglyf/mpdemo/mapper/UserMapper.java
+++ src/main/java/com/jxglyf/mpdemo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.jxglyf.mpdemo.mapper;
 
 import com.jxglyf.mpdemo.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,5 +13,9 @@ public interface UserMapper {
     // Query all the user
     @Select("SELECT * FROM user")
     public List<User> find();
+
+    // Insert one user
+    @Insert("INSERT INTO user VALUES (#{id}, #{username}, #{password}, #{birthday})")
+    public int insert(User user);
 
 }
